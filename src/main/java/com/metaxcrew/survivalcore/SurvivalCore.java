c17d5f53951f600253b4fe986f1b1e72a7830fd7
Mat <silencekrakens@gmail.com>
2022-04-05 02:06:26 +0200

    Removed CI command/Listener

--- src/main/java/com/metaxcrew/survivalcore/SurvivalCore.java
+++ src/main/java/com/metaxcrew/survivalcore/SurvivalCore.java
@@ -4,7 +4,6 @@ import com.metaxcrew.survivalcore.commands.HatCommand;
 import com.metaxcrew.survivalcore.commands.NightVisionCommand;
 import com.metaxcrew.survivalcore.core.ScoreBoard;
 import com.metaxcrew.survivalcore.events.JoinLeaveEvent;
-import com.metaxcrew.survivalcore.guis.CustomItems;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SurvivalCore extends JavaPlugin {
@@ -14,10 +13,8 @@ public void onEnable() {
     instance = this;
     getServer().getPluginManager().registerEvents(new ScoreBoard(), this);
     getServer().getPluginManager().registerEvents(new JoinLeaveEvent(), this);
-    getServer().getPluginManager().registerEvents(new CustomItems(), this);
 
 
-    getCommand("ci").setExecutor(new CustomItems());
     getCommand("nv").setExecutor(new NightVisionCommand());
     getCommand("hat").setExecutor(new HatCommand());
 
